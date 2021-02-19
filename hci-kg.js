f580c2648d6708bf0614a3b3a55efe57c3b8655c
Marie <marie@ototoi.fr>
2021-02-19 22:05:38 +0100

    Add practical information

--- hci-kg.js
+++ hci-kg.js
@@ -1,6 +1,9 @@
-$(document).ready(function() {
-    $("header nav li a, header h1 a").each(function(e){
-        var prevhref = $(this).attr("href")
-        $(this).attr("href", prevhref + '?' + Math.random())
-    })
-})
+$(document).ready(function () {
+  $("header nav li a, header h1 a").each(function (e) {
+    var prevhref = $(this).attr("href");
+    $(this).attr(
+      "href",
+      prevhref + "?" + Math.random().toString().substr(0, 7)
+    );
+  });
+});
